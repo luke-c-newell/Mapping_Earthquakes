ae43f77f400f8a1857bd71baa64169bd287251c5
Luke Newell <lukenewell@lukes-mbp.home>
2021-01-14 18:18:11 -0600

    Added feature to map GeoJSON points to a map

--- Mapping_GeoJSON_Points/static/js/logic.js
+++ Mapping_GeoJSON_Points/static/js/logic.js
@@ -1,32 +1,58 @@
 // Add console.log to check to see if our code is working.
 console.log("working");
 
-// Create the map object with a center at SFO.
-let map = L.map('mapid').setView([39.0561, -95.6953], 4);
+// Create the map object with center at the San Francisco airport.
+let map = L.map('mapid').setView([37.618, -122.375], 11);
 
-// Coordinates for each airport to be used in the polyline.
-let line = [
-    [37.6213, -122.3790], // SFO
-    [30.1900, -97.6687],  // AUS
-    [39.2980, -94.7170],  // KCI
-    [43.6777, -79.6248],  // YYZ
-    [40.6413, -73.7781]   // JFK
-  ];  
+// Add GeoJSON data.
+let sanFranAirport =
+{"type":"FeatureCollection","features":[{
+    "type":"Feature",
+    "properties":{
+        "id":"3469",
+        "name":"San Francisco International Airport",
+        "city":"San Francisco",
+        "country":"United States",
+        "faa":"SFO",
+        "icao":"KSFO",
+        "alt":"13",
+        "tz-offset":"-8",
+        "dst":"A",
+        "tz":"America/Los_Angeles"},
+        "geometry":{
+            "type":"Point",
+            "coordinates":[-122.375,37.61899948120117]}}
+]};
 
-// Create a polyline using the line coordinates and make the line red.
-L.polyline(line, {
-    color: "blue",
-    weight: 4,
-    opacity: 0.5,
-    dashArray: '4, 10'
-  }).addTo(map);
+// Grabbing our GeoJSON data and using the onEachFeature function.
+L.geoJson(sanFranAirport, {
+  // We turn each feature into a marker on the map.
+  onEachFeature: function(feature, layer) {
+    console.log(layer);
+    layer.bindPopup("<h2>" + "Airport Code: " + feature.properties.faa + "</h2> <hr> <h3>Airport Name: " + feature.properties.name + "</h3>");
+  }
+
+}).addTo(map);
+
+// // Grabbing our GeoJSON data and using the pointToLayer function.
+// L.geoJson(sanFranAirport, {
+//   // We turn each feature into a marker on the map.
+//   pointToLayer: function(feature, latlng) {
+//     console.log(feature);
+//     return L.marker(latlng)
+//     .bindPopup("<h2>" + feature.properties.name + "</h2> <hr> <h3>" + feature.properties.city + ", " + feature.properties.country + "</h3>");
+//   }
+
+// }).addTo(map);
 
 // We create the tile layer that will be the background of our map.
-let light = L.tileLayer('https://api.mapbox.com/styles/v1/mapbox/light-v10/tiles/{z}/{x}/{y}?access_token={accessToken}', {
+let navPreview = L.tileLayer('https://api.mapbox.com/styles/v1/mapbox/outdoors-v11/tiles/{z}/{x}/{y}?access_token={accessToken}', {
 attribution: 'Map data © <a href="https://www.openstreetmap.org/">OpenStreetMap</a> contributors, <a href="https://creativecommons.org/licenses/by-sa/2.0/">CC-BY-SA</a>, Imagery (c) <a href="https://www.mapbox.com/">Mapbox</a>',
     maxZoom: 18,
     accessToken: API_KEY
 });
 
 // Then we add our tile layer to the map.
-light.addTo(map);
+navPreview.addTo(map);
+
+// Tile Layers used: outdoors-v11, navigation-preview-night-v2
